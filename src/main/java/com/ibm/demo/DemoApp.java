5e12731f48f32392c776d2be6d0defa54d99f712
JackSilence <jacksilence@gmail.com>
2018-01-28 16:03:15 +0800

    includeFlat加入utils; 調整build.gradle & settings.gradle

--- src/main/java/com/ibm/demo/DemoApp.java
+++ src/main/java/com/ibm/demo/DemoApp.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import com.ibm.util.Utils;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class DemoApp {
 	public static void main( String[] args ) {
+		System.out.println( Utils.sayHello() );
+
 		SpringApplication.run( DemoApp.class, args );
 	}
 }
